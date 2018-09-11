f1286a7fe3a8ccc6183b8cb27aa4cf22d451387a
Sergey Veselkov <veselkovsd@yandex.ru>
2018-09-11 22:07:27 +0300

    display pos on click

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,79 @@
 extern crate bincode;
 #[macro_use]
 extern crate conrod;
-extern crate gfx_window_glutin;
-extern crate glutin;
 extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate winit;
 
 mod map;
 
-use conrod::color::Color;
-use conrod::gfx::Device;
-use conrod::widget;
-use conrod::Labelable;
-use conrod::Positionable;
-use conrod::Sizeable;
-use conrod::Widget;
-use glutin::GlContext;
+use conrod::backend::glium::glium::{self, Surface};
+use conrod::{color, widget, Colorable, Positionable, Widget};
 use map::{Map, MapPos};
 
-const CLEAR_COLOR: [f32; 4] = [0.2, 0.2, 0.2, 1.0];
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 600;
+
+/// In most of the examples the `glutin` crate is used for providing the window context and
+/// events while the `glium` crate is used for displaying `conrod::render::Primitives` to the
+/// screen.
+///
+/// This `Iterator`-like type simplifies some of the boilerplate involved in setting up a
+/// glutin+glium event loop that works efficiently with conrod.
+pub struct EventLoop {
+    ui_needs_update: bool,
+    last_update: std::time::Instant,
+}
+
+impl EventLoop {
+    pub fn new() -> Self {
+        EventLoop {
+            last_update: std::time::Instant::now(),
+            ui_needs_update: true,
+        }
+    }
+
+    /// Produce an iterator yielding all available events.
+    pub fn next(
+        &mut self,
+        events_loop: &mut glium::glutin::EventsLoop,
+    ) -> Vec<glium::glutin::Event> {
+        // We don't want to loop any faster than 60 FPS, so wait until it has been at least 16ms
+        // since the last yield.
+        let last_update = self.last_update;
+        let sixteen_ms = std::time::Duration::from_millis(16);
+        let duration_since_last_update = std::time::Instant::now().duration_since(last_update);
+        if duration_since_last_update < sixteen_ms {
+            std::thread::sleep(sixteen_ms - duration_since_last_update);
+        }
+
+        // Collect all pending events.
+        let mut events = Vec::new();
+        events_loop.poll_events(|event| events.push(event));
+
+        // If there are no events and the `Ui` does not need updating, wait for the next event.
+        if events.is_empty() && !self.ui_needs_update {
+            events_loop.run_forever(|event| {
+                events.push(event);
+                glium::glutin::ControlFlow::Break
+            });
+        }
+
+        self.ui_needs_update = false;
+        self.last_update = std::time::Instant::now();
+
+        events
+    }
+
+    /// Notifies the event loop that the `Ui` requires another update whether or not there are any
+    /// pending events.
+    ///
+    /// This is primarily used on the occasion that some part of the `Ui` is still animating and
+    /// requires further updates to do so.
+    pub fn needs_update(&mut self) {
+        self.ui_needs_update = true;
+    }
+}
 
 fn main() {
     let mut m = Map::new(5, 5);
@@ -36,123 +89,96 @@ fn main() {
     println!("");
     m.print_path(vec);
 
-    let builder = glutin::WindowBuilder::new()
-        .with_title("Поиск кратчайшего пути")
-        .with_resizable(false)
-        .with_dimensions((800, 600).into());
-    let context = glutin::ContextBuilder::new().with_multisampling(4);
+    // Build the window.
+    let mut events_loop = glium::glutin::EventsLoop::new();
+    let window = glium::glutin::WindowBuilder::new()
+        .with_title("Hello Conrod!")
+        .with_dimensions((WIDTH, HEIGHT).into());
+    let context = glium::glutin::ContextBuilder::new()
+        .with_vsync(true)
+        .with_multisampling(4);
+    let display = glium::Display::new(window, context, &events_loop).unwrap();
 
-    let mut events_loop = winit::EventsLoop::new();
+    // construct our `Ui`.
+    let mut ui = conrod::UiBuilder::new([WIDTH as f64, HEIGHT as f64]).build();
 
-    // Initialize gfx things
-    let (window, mut device, mut factory, rtv, _) = gfx_window_glutin::init::<
-        conrod::backend::gfx::ColorFormat,
-        conrod::gfx_core::format::DepthStencil,
-    >(builder, context, &events_loop);
-
-    let mut encoder: conrod::gfx::Encoder<_, _> = factory.create_command_buffer().into();
-
-    let mut renderer =
-        conrod::backend::gfx::Renderer::new(&mut factory, &rtv, window.get_hidpi_factor() as f64)
-            .unwrap();
-
-    let mut ui = conrod::UiBuilder::new([800 as f64, 600 as f64]).build();
-
-    // Instantiate the generated list of widget identifiers.
-    widget_ids!(struct Ids { canvas, counter });
+    // Generate the widget identifiers.
+    widget_ids!(struct Ids { text, matrix });
     let ids = Ids::new(ui.widget_id_generator());
 
-    let mut image_map = conrod::image::Map::new();
+    // A type used for converting `conrod::render::Primitives` into `Command`s that can be used
+    // for drawing to the glium `Surface`.
+    let mut renderer = conrod::backend::glium::Renderer::new(&display).unwrap();
 
-    renderer.clear(&mut encoder, CLEAR_COLOR);
-    encoder.flush(&mut device);
-    window.swap_buffers().unwrap();
-    device.cleanup();
+    // The image map describing each of our widget->image mappings (in our case, none).
+    let image_map = conrod::image::Map::<glium::texture::Texture2d>::new();
 
-    let mut count = 0;
+    // Poll events from the window.
+    let mut event_loop = EventLoop::new();
 
+    let mut pos = glium::glutin::dpi::LogicalPosition::new(0f64, 0f64);
     'main: loop {
-        let mut should_quit = false;
-        let (win_w, win_h): (u32, u32) = match window.get_inner_size() {
-            Some(s) => s.into(),
-            None => break 'main,
-        };
-        let dpi_factor = window.get_hidpi_factor() as f32;
-        events_loop.poll_events(|event| {
-            // Convert winit event to conrod event, requires conrod to be built with the `winit` feature
-            if let Some(event) =
-                conrod::backend::winit::convert_event(event.clone(), window.window())
-            {
+        // Handle all events.
+        for event in event_loop.next(&mut events_loop) {
+            // Use the `winit` backend feature to convert the winit event to a conrod one.
+            if let Some(event) = conrod::backend::winit::convert_event(event.clone(), &display) {
                 ui.handle_event(event);
+                event_loop.needs_update();
             }
-            // Close window if the escape key or the exit button is pressed
+
             match event {
-                winit::Event::WindowEvent { event, .. } => match event {
-                    winit::WindowEvent::KeyboardInput {
+                glium::glutin::Event::WindowEvent { event, .. } => match event {
+                    // Break from the loop upon `Escape`.
+                    glium::glutin::WindowEvent::CloseRequested
+                    | glium::glutin::WindowEvent::KeyboardInput {
                         input:
-                            winit::KeyboardInput {
-                                virtual_keycode: Some(winit::VirtualKeyCode::Escape),
+                            glium::glutin::KeyboardInput {
+                                virtual_keycode: Some(glium::glutin::VirtualKeyCode::Escape),
                                 ..
                             },
                         ..
+                    } => break 'main,
+                    glium::glutin::WindowEvent::CursorMoved { position, .. } => {
+                        pos = position;
                     }
-                    | winit::WindowEvent::CloseRequested => should_quit = true,
-                    _ => {}
+                    glium::glutin::WindowEvent::MouseInput {
+                        state: glium::glutin::ElementState::Pressed,
+                        ..
+                    } => {
+                        let (x, y): (i32, i32) = pos.into();
+                        println!("Pressed: x: {}, y: {}", x, y);
+                    }
+                    _ => (),
                 },
-                _ => {}
+                _ => (),
             }
+        }
 
-            // Instantiate all widgets in the GUI.
-            {
-                let ui = &mut ui.set_widgets();
+        // Set the widgets.
+        let ui = &mut ui.set_widgets();
 
-                // Create a background canvas upon which we'll place the button.
-                widget::Canvas::new().pad(40.0).set(ids.canvas, ui);
+        let mut elements = widget::Matrix::new(5, 4)
+            .middle_of(ui.window)
+            .set(ids.matrix, ui);
 
-                // Draw the button and increment `count` if pressed.
-                for _click in widget::Button::new()
-                    .middle_of(ids.canvas)
-                    .w_h(80.0, 80.0)
-                    .label(&count.to_string())
-                    .label_color(Color::Rgba(255f32, 0f32, 0f32, 0f32))
-                    .set(ids.counter, ui)
-                {
-                    count += 1;
-                }
-            }
-
-            // Render the `Ui` and then display it on the screen.
-            /* if let Some(primitives) = ui.draw_if_changed() {
-                renderer.fill(&display, primitives, &image_map);
-                let mut target = display.draw();
-                target.clear_color(0.0, 0.0, 0.0, 1.0);
-                renderer.draw(&display, &mut target, &image_map).unwrap();
-                target.finish().unwrap();
+        while let Some(elem) = elements.next(ui) {
+            let (r, c) = (elem.row, elem.col);
+            let n = c + r * c;
+            let luminance = n as f32 / (5 * 4) as f32;
+            let canvas = widget::Canvas::new().color(color::BLUE.with_luminance(luminance));
+            elem.set(canvas, ui);
+            /* for _click in elem.set(button, ui) {
+                println!("Hey! {:?}", (r, c));
             } */
-            if let Some(primitives) = ui.draw_if_changed() {
-                let dims = (win_w as f32 * dpi_factor, win_h as f32 * dpi_factor);
+        }
 
-                //Clear the window
-                renderer.clear(&mut encoder, CLEAR_COLOR);
-
-                renderer.fill(
-                    &mut encoder,
-                    dims,
-                    dpi_factor as f64,
-                    primitives,
-                    &image_map,
-                );
-
-                renderer.draw(&mut factory, &mut encoder, &image_map);
-
-                encoder.flush(&mut device);
-                window.swap_buffers().unwrap();
-                device.cleanup();
-            }
-        });
-        if should_quit {
-            break 'main;
+        // Render the `Ui` and then display it on the screen.
+        if let Some(primitives) = ui.draw_if_changed() {
+            renderer.fill(&display, primitives, &image_map);
+            let mut target = display.draw();
+            target.clear_color(0.0, 0.0, 0.0, 1.0);
+            renderer.draw(&display, &mut target, &image_map).unwrap();
+            target.finish().unwrap();
         }
     }
-    println!("{}", count);
 }
