ea4ce6e131b89c78bec1424f5567a036dd582df0
Sergey Veselkov <veselkovsd@yandex.ru>
2018-10-24 23:09:36 +0300

    update

--- src/gui/map_grid.rs
+++ src/gui/map_grid.rs
@@ -13,15 +13,33 @@ struct Color {
     blue: f64,
 }
 
+impl Color {
+    fn white() -> Self {
+        Self {
+            red: 1f64,
+            green: 1f64,
+            blue: 1f64,
+        }
+    }
+
+    fn yellow() -> Self {
+        Self {
+            red: 1f64,
+            green: 1f64,
+            blue: 0f64,
+        }
+    }
+}
+
 #[derive(Debug)]
-struct Point {
+struct Coordinate {
     x: usize,
     y: usize,
 }
 
 #[derive(Debug)]
 struct PointPath {
-    points: Vec<Point>,
+    coordinates: Vec<Coordinate>,
     color: Color,
     width: f64,
 }
@@ -49,14 +67,7 @@ impl Widget for MapGrid {
     fn model(size: (usize, usize)) -> Model {
         Model {
             draw_handler: DrawHandler::new().expect("draw handler"),
-            cell: vec![
-                Color {
-                    red: 1f64,
-                    green: 1f64,
-                    blue: 1f64
-                };
-                size.0 * size.1
-            ],
+            cell: vec![Color::white(); size.0 * size.1],
             width: size.0,
             path: None,
         }
@@ -74,6 +85,7 @@ impl Widget for MapGrid {
                 let cell_width = allocation.width as f64 / self.model.width as f64;
                 let cell_height =
                     allocation.height as f64 / (self.model.cell.len() / self.model.width) as f64;
+                // draw grid
                 let mut cell_x = 0f64;
                 let mut cell_y = 0f64;
                 let border = 1f64;
@@ -91,6 +103,34 @@ impl Widget for MapGrid {
                     context.set_source_rgb(c.red, c.green, c.blue);
                     context.fill();
                     cell_x += cell_width;
+                }
+
+                /* self.model.path = Some(PointPath {
+                    coordinates: vec![
+                        Coordinate { x: 0, y: 0 },
+                        Coordinate { x: 1, y: 1 },
+                        Coordinate { x: 1, y: 2 },
+                    ],
+                    color: Color::yellow(),
+                    width: 4f64,
+                }); */
+                // draw path
+                if let Some(ref path) = self.model.path {
+                    if path.coordinates.len() > 1 {
+                        context.set_line_width(path.width);
+                        context.set_source_rgb(path.color.red, path.color.green, path.color.blue);
+                        context.move_to(
+                            (path.coordinates[0].x as f64 + 0.5f64) * cell_width,
+                            (path.coordinates[0].y as f64 + 0.5f64) * cell_height,
+                        );
+                        for c in path.coordinates.iter().skip(1) {
+                            context.line_to(
+                                (c.x as f64 + 0.5f64) * cell_width,
+                                (c.y as f64 + 0.5f64) * cell_height,
+                            );
+                        }
+                        context.stroke();
+                    }
                 }
             }
         }
